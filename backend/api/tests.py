33caa2e04636ed3d44d638f62b71bd424fa6d7e3
ddmikhaylov <koolzeeqne@yandex.ru>
2023-07-26 19:39:30 +0300

    blump5

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,9 +1,9 @@
 # backend/api/tests.py
-from api import models
-
 from http import HTTPStatus
 
+from api import models
 from django.test import Client, TestCase
+
 
 class TaskiAPITestCase(TestCase):
     def setUp(self):
