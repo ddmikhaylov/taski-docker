9e58384023535986a38c089c642c4828135a3f35
ddmikhaylov <119514567+ddmikhaylov@users.noreply.github.com>
2023-07-26 19:24:07 +0300

    Update tests.py

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -1,7 +1,8 @@
 # backend/api/tests.py
+from api import models
+
 from http import HTTPStatus
 
-from api import models
 from django.test import Client, TestCase
 
 class TaskiAPITestCase(TestCase):
